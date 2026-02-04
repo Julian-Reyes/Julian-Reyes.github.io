c33c8688e61b8d91630db1f2e32fde9e06e63045
Julian-Reyes <julian.edward.reyes@gmail.com>
2026-02-04 19:13:25 -0300

    fixed navbar on homepage
    ✅ index.html fixed navbar (added tailwind navbar from projects page)
    ✅ refactored homepage to match bootstrap formatting with the new tailwind
    ✅ added 100px margin-bottom to #molecular on the highlighted skills to fix breaking the site when sizing to smaller screens

--- index/assets/js/main.js
+++ index/assets/js/main.js
@@ -1,3 +1,15 @@
+// Get references to the button and the menu
+const menuBtn = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+// Toggle the 'hidden' class when the button is clicked
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+
+
+
 /*
 	Spatial by TEMPLATED
 	templated.co @templatedco
